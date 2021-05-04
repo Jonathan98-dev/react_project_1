d59cc7042a18c902738b31bc3bf1a6feb2ee78ce
Jonathan98-dev <email.jonathannuel@gmail.com>
2021-05-04 21:44:43 +0200

    done for today 4.5.21

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.scss";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 
-function App() {
-  return <div className="App">asdasd</div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import "./navbarStyle.scss";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <h1>Navbar</h1>
+        <div className="links">
+          <a href="/">PLACEHOLDER</a>
+          <a href="/create">PLACEHOLDER</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
