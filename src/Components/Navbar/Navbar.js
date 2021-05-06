6271c6621568541539329b37465762437e976169
Jonathan98-dev <email.jonathannuel@gmail.com>
2021-05-06 09:54:55 +0200

    finished navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,10 +3,13 @@ import "./navbarStyle.scss";
 const Navbar = () => {
   return (
     <nav className="navbarComponent">
-      <h1>Navbar</h1>
+      <a className="logo" href="/#">
+        <img src="../../img/logo.png" alt="My Logo" />
+      </a>
       <div className="links">
-        <a href="/">PLACEHOLDER</a>
-        <a href="/create">PLACEHOLDER</a>
+        <a href="/#">Home</a>
+        <a href="/#">About me</a>
+        <a href="/#">Message me</a>
       </div>
     </nav>
   );
